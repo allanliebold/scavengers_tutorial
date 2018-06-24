20d12cf5645dd58d6daf25a02aa9c092473e56b4
Allan Liebold <allan.liebold@gmail.com>
2018-06-24 10:20:30 -0700

    Update BoardManager.cs

--- scripts/BoardManager.cs
+++ scripts/BoardManager.cs
@@ -40,6 +40,8 @@ public class BoardManager : MonoBehaviour {
     for (int x = -1; x < columns + 1; x++) {
       for (int y = -1; y < rows + 1; y++) {
         GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
+        if (x == -1 || x == columns || y == -1 || y == rows)
+          toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
       }
     }
     
