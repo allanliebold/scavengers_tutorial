2b12d5e14381a91e50c16bc43187c819f10a3fa7
Allan Liebold <allan.liebold@gmail.com>
2018-06-24 10:11:50 -0700

    Update BoardManager.cs

--- scripts/BoardManager.cs
+++ scripts/BoardManager.cs
@@ -7,6 +7,10 @@ public class BoardManager : MonoBehaviour {
   [Serializable]
   public class Count {
     public int minimum, maximum;
+    public Count (int min, int max) {
+      minimum = min;
+      maximum = max;
+    }
   }
   
 }
