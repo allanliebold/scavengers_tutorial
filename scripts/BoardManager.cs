77bb9469d6808595b0ed7c770bc5d9cfb31daf1c
Allan Liebold <allan.liebold@gmail.com>
2018-06-24 10:12:43 -0700

    Update BoardManager.cs

--- scripts/BoardManager.cs
+++ scripts/BoardManager.cs
@@ -16,5 +16,6 @@ public class BoardManager : MonoBehaviour {
   public int columns = 8;
   public int rows = 8;
   public Count wallCount = new Count (5,9);
+  public foodCount = new Count (1, 5);
   
 }
